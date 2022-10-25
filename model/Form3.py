c721d272079d14631446b5bebf9530eceb604c9d
cojocariudanie1 <cojocariudaniel02@gmail.com>
2022-10-25 18:21:57 +0300

    update

---
+++ model/Form3.py
@@ -0,0 +1,90 @@
+import random
+
+import matplotlib
+from matplotlib import pyplot as plt
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas, \
+    NavigationToolbar2QT as NavigationToolbar
+from matplotlib.figure import Figure
+from PyQt5 import QtCore, QtWidgets
+
+from model.statistic_functions.form3_functions import get_sales, scoatere_produs, get_products, get_category, \
+    scoatere_categorie
+from views.form3_view import Ui_MainWindow
+from matplotlib import rcParams
+
+rcParams.update({'figure.autolayout': True})
+matplotlib.use('Qt5Agg')
+
+
+class MplCanvas(FigureCanvas):
+    def __init__(self, parent=None, width=5, height=4, dpi=300):
+        self.fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = self.fig.add_subplot(111)
+        super(MplCanvas, self).__init__(self.fig)
+        self.fig.tight_layout()
+
+
+class Form3(QtWidgets.QMainWindow):
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.sc = MplCanvas(self, width=5, height=4, dpi=100)
+        self.toolbar = NavigationToolbar(self.sc, self)
+        self.ui.chart_layout.addWidget(self.sc)
+        self.ui.chart_layout.addWidget(self.toolbar)
+
+        self.ui.produs_button.clicked.connect(self.produs_button)
+        self.ui.clasaPrdus_button.clicked.connect(self.categorie_button)
+        self.ui.sales_label_pr.setText(f"Sales: 0")
+        self.ui.sales_label_clpr.setText(f"Sales: 0")
+        self.ui.sales.setText(str(get_sales()))
+        self.populate_combobox()
+
+
+    def populate_combobox(self):
+        product_category = get_category()
+        for k in product_category:
+            self.ui.clasaProdus_CB.addItem(str(k))
+
+        prducts = get_products()
+        for j in prducts:
+            self.ui.ProdusCB.addItem(str(j))
+
+
+    def categorie_button(self):
+        self.sc.axes.clear()
+        obj = scoatere_categorie(self.ui.clasaProdus_CB.currentText())
+        date = obj[0]
+        sum = obj[1]
+        max= obj[2]
+        total_sum = obj[3]
+        if int(self.ui.sales.text()) > total_sum:
+            self.ui.profit_sau_pierdere.setText(f"Pierdere: {str(int(self.ui.sales.text()) - total_sum)}")
+        else:
+            self.ui.profit_sau_pierdere.setText(f"Profit: {str(total_sum - int(self.ui.sales.text()))}")
+
+        self.ui.sales_label_clpr.setText(f"Sales: {total_sum}")
+        self.sc.axes.bar(date,sum)
+        self.sc.axes.bar(date,max)
+        self.sc.draw()
+        self.show()
+
+    def produs_button(self):
+        self.sc.axes.clear()
+        obj = scoatere_produs(self.ui.ProdusCB.currentText())
+        date = obj[0]
+        sum = obj[1]
+        max = obj[2]
+        total_sum = obj[3]
+        if int(self.ui.sales.text()) > total_sum:
+            self.ui.profit_sau_pierdere.setText(f"Pierdere: {str(int(self.ui.sales.text()) - total_sum)}")
+        else:
+            self.ui.profit_sau_pierdere.setText(f"Profit: {str(total_sum - int(self.ui.sales.text()))}")
+
+        self.ui.sales_label_pr.setText(f"Sales: {total_sum}")
+
+        self.sc.axes.bar(date,sum)
+        self.sc.axes.bar(date,max)
+        self.sc.draw()
+        self.show()
---
+++ model/statistic_functions/form3_functions.py
@@ -0,0 +1,70 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+from matplotlib.pyplot import plot
+
+
+def get_data():
+    file_name = "Superbaza.xls"  # path to file + file name
+    sheet = "superstore"  # sheet name or sheet number or list of sheet numbers and names
+    df = pd.read_excel(io=file_name, sheet_name=sheet, usecols="A:U")
+
+    dataframe = pd.DataFrame(df)
+
+    return dataframe
+
+
+def get_sales():
+    df = get_data()
+    sales = df["Sales"].sum()
+    return int(sales)
+
+
+def get_products():
+    dataframe = get_data()
+    all_products = dataframe["Product Name"].drop_duplicates()
+    return all_products
+
+
+def get_category():
+    dataframe = get_data()
+    category = dataframe["Category"].drop_duplicates()
+    return category
+
+
+def new_sales(product_name):
+    print(product_name)
+    df = get_data()
+    print(df["Sales"].sum())
+    total_sum = df[df["Product Name"] != product_name].sum()
+    print(total_sum["Sales"])
+    return int(total_sum["Sales"])
+
+
+def scoatere_categorie(categorie_var):
+    df = get_data()
+    category_df = df[["Category", "Sales", "Order Date"]]
+    uq_category = category_df[category_df["Category"] != str(categorie_var)]
+    df2 = uq_category.groupby(uq_category["Order Date"].dt.year).agg(['sum', 'max'])
+    total_sum = uq_category["Sales"].sum()
+    date = df2.index
+    sum = df2["Sales"]["sum"].tolist()
+    max = df2["Sales"]["max"].tolist()
+
+    obj = [date, sum, max, int(total_sum)]
+    return obj
+
+
+def scoatere_produs(product_name):
+    df = get_data()
+    produse1 = df[["Order Date", "Sales", "Product Name", "Category"]]
+    uq_produs = produse1[produse1["Product Name"] != str(product_name)]
+    df2 = uq_produs.groupby(uq_produs["Order Date"].dt.year).agg(['sum', 'max'])
+
+    total_sum = new_sales(product_name)
+
+    date = df2.index
+    sum = df2["Sales"]["sum"].tolist()
+    max = df2["Sales"]["max"].tolist()
+
+    obj = [date, sum, max, total_sum]
+    return obj
